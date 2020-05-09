d3dfeea332d0663a186d7b560a955d8d7182cd78
Aryan Amlathe <35621477+Amlathearyan@users.noreply.github.com>
2020-05-09 19:11:19 +0000

    Add files via upload

---
+++ Age Calculator/agecalculator.js
@@ -0,0 +1,11 @@
+/*
+var age = prompt("What is your age?");
+no= age * 365 ;
+leap = age%4 ; 
+final = no + leap ;
+alert("You lived " +final+ "days" );
+*/
+
+var age = prompt("What is your age?");
+days= age * 365.25 ;
+alert("You lived " +days+ "days" );
